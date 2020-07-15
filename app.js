9bda94471d781c32783868a94096808fb4a0e888
jsdnlb <danielb.alzate1@gmail.com>
2020-07-15 14:47:52 -0500

    Implementando express y nodemon

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app = express();
+
+app.use("/static", express.static("public"));
+
+app.get("/", function (req, res) {
+  res.send("Hello World!");
+});
+
+app.get("/home", function (req, res) {
+  res.send("Home!");
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
