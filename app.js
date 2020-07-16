2efaf9fb424bbe4f355c2903587d3b84f20085d7
jsdnlb <danielb.alzate1@gmail.com>
2020-07-15 20:03:53 -0500

    Implementando handlebars

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 var express = require("express");
 var app = express();
+var exphbs = require("express-handlebars");
 
-app.use("/static", express.static("public"));
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+app.use("/static", express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
-  res.send("Hello World!");
+  res.render("home", {
+    title: "Bienvenido",
+    description: "Gracias por visitar esta página",
+  });
 });
 
-app.get("/home", function (req, res) {
-  res.send("Home!");
+app.get("/about", function (req, res) {
+  res.render("home", {
+    title: "Acerca de",
+    description: "Somos expertos en aprender",
+  });
 });
 
 app.listen(3000, function () {
