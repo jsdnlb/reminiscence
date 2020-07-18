d9bb899534ecb4e39470d94ba9087301cd8056df
jsdnlb <danielb.alzate1@gmail.com>
2020-07-18 11:41:53 -0500

    Ignorando configuración privada Firebase

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 var express = require("express");
 var app = express();
 var exphbs = require("express-handlebars");
+var admin = require("firebase-admin");
 
 app.engine("handlebars", exphbs());
 app.set("view engine", "handlebars");
-
 app.use("/static", express.static(__dirname + "/public"));
+
+// Fetch the service account key JSON file contents
+var serviceAccount = require("./reminiscenceKey.json");
+
+// Initialize the app with a service account, granting admin privileges
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://databaseName.firebaseio.com",
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function (snapshot) {
+  console.log(snapshot.val());
+});
 
 app.get("/", function (req, res) {
   res.render("home", {
@@ -15,6 +31,14 @@ app.get("/", function (req, res) {
 });
 
 app.get("/about", function (req, res) {
+  res.render("home", {
+    title: "Acerca de",
+    description: "Somos expertos en aprender",
+  });
+});
+
+app.post("/save", function (req, res) {
+  console.log(req);
   res.render("home", {
     title: "Acerca de",
     description: "Somos expertos en aprender",
